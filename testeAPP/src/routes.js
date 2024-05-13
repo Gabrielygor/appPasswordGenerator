1fabbe67291b2dc2e77d44498ed0fdf636b32110
Gabrielygor <gabrielcanuto042@gmail.com>
2024-05-13 07:55:23 -0300

    Add Icons

--- testeAPP/src/routes.js
+++ testeAPP/src/routes.js
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Home } from './pages/home'
 import { Passwords } from './pages/passwords'
+import { Ionicons } from '@expo/vector-icons'
+
 
 
 const Tab = createBottomTabNavigator();
@@ -12,11 +14,36 @@ export function Routes() {
             <Tab.Screen
                 name="Home"
                 component={Home}
+                options={{
+
+                    headerShown: false,
+                    tabBarIcon: ({ focused, size, color }) => {
+                        if (focused) {
+                            return <Ionicons size={size} color={color} name='home' />
+                        }
+
+                        return <Ionicons size={size} color={color} name='home-outline' />
+                    }
+
+                }}
             />
 
             <Tab.Screen
                 name="Passwords"
                 component={Passwords}
+                options={{
+
+                    headerShown: false,
+                    tabBarIcon: ({ focused, size, color }) => {
+                        if (focused) {
+                            return <Ionicons size={size} color={color} name='lock-closed' />
+                        }
+                        
+                        return <Ionicons size={size} color={color} name='lock-closed-outline' />
+
+                    }
+
+                }}
             />
         </Tab.Navigator>
 
